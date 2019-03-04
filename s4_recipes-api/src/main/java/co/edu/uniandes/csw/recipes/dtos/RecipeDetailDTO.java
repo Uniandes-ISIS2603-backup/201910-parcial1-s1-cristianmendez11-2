b48d067fd7e4eb9d0842744db046333847a84c76
Cristian Mendez <c.mendez11@uniandes.edu.co>
2019-03-04 18:36:33 -0500

    (5%) Defina un atributo nuevo en RecipeDetailDTO que representa el listado de ingredientes de la receta. Defina set/get y actualice el método constructor que recibe un RecipeEntity al igual que el método toEntity, el cual retorna un objeto de tipo RecipeEntity, para que también hagan la conversión del listado de ingredientes.

--- s4_recipes-api/src/main/java/co/edu/uniandes/csw/recipes/dtos/RecipeDetailDTO.java
+++ s4_recipes-api/src/main/java/co/edu/uniandes/csw/recipes/dtos/RecipeDetailDTO.java
@@ -5,20 +5,43 @@
  */
 package co.edu.uniandes.csw.recipes.dtos;
 
+import co.edu.uniandes.csw.recipes.entities.IngredientEntity;
 import co.edu.uniandes.csw.recipes.entities.RecipeEntity;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author CesarF
  */
 public class RecipeDetailDTO extends RecipeDTO {
-    
+    private List<IngredientDTO> ingredient;
     public RecipeDetailDTO(){
     
     }
     
     public RecipeDetailDTO(RecipeEntity entity){
         super(entity);
+        if (entity.getIngredient()!= null) {
+            ingredient = new ArrayList<>();
+            for (IngredientEntity entityIngredient : entity.getIngredient()) {
+                ingredient.add(new IngredientDTO(entityIngredient));
+            }
+        }
+    }
+
+    /**
+     * @return the ingredient
+     */
+    public List<IngredientDTO> getIngredient() {
+        return ingredient;
+    }
+
+    /**
+     * @param ingredient the ingredient to set
+     */
+    public void setIngredient(List<IngredientDTO> ingredient) {
+        this.ingredient = ingredient;
     }
     
 }
--- s4_recipes-back/src/main/java/co/edu/uniandes/csw/recipes/entities/RecipeEntity.java
+++ s4_recipes-back/src/main/java/co/edu/uniandes/csw/recipes/entities/RecipeEntity.java
@@ -6,7 +6,12 @@
 package co.edu.uniandes.csw.recipes.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import uk.co.jemos.podam.common.PodamExclude;
 
 /**
  *
@@ -16,7 +21,9 @@ import javax.persistence.Entity;
 public class RecipeEntity extends BaseEntity implements Serializable{
     private String name;
     private String description;
-    
+    @PodamExclude
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<IngredientEntity> ingredient   = new ArrayList<IngredientEntity>();
     public RecipeEntity(){
     
     }
@@ -46,6 +53,20 @@ public class RecipeEntity extends BaseEntity implements Serializable{
      */
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    /**
+     * @return the ingredient
+     */
+    public List<IngredientEntity> getIngredient() {
+        return ingredient;
+    }
+
+    /**
+     * @param ingredient the ingredient to set
+     */
+    public void setIngredient(List<IngredientEntity> ingredient) {
+        this.ingredient = ingredient;
     }
     
     
